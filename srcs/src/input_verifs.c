882f059c0d897dd0b21c2ddf922c0dc27179675f
ppitzini <jeanbblo4588@gmail.com>
2024-07-08 19:20:55 +0200

    ss

--- srcs/src/input_verifs.c
+++ srcs/src/input_verifs.c
@@ -6,7 +6,7 @@
 /*   By: ppitzini <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 16:22:08 by ppitzini          #+#    #+#             */
-/*   Updated: 2024/07/08 17:48:39 by ppitzini         ###   ########.fr       */
+/*   Updated: 2024/07/08 18:55:07 by ppitzini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,22 @@ int	ac_checks(int ac)
 
 int	extension(char *str, char *ext)
 {
-    int	i;
-    int	j;
+	int	i;
+	int	j;
 
-    i = ft_strlen(str) - 1;
-    j = ft_strlen(ext) - 1;
-    while (i >= 0 && j >= 0)
-    {
-        if (str[i] != ext[j])
-            return (1);
-        i--;
-        j--;
-    }
-    // If we've gone through the entire extension and every character matched,
-    // return 0 only if we've also gone through the entire extension.
-    // This ensures that the filename isn't something like "file.pngextra".
-    return (j < 0) ? 0 : 1;
+	i = ft_strlen(str) - 1;
+	j = ft_strlen(ext) - 1;
+	while (i >= 0 && j >= 0)
+	{
+		if (str[i] != ext[j])
+			return (1);
+		i--;
+		j--;
+	}
+	if (j < 0)
+		return (0);
+	else
+		return (1);
 }
 
 int	first_checks_hub(int ac, char **av, t_core *c)
